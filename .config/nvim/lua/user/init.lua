a2d1fd54ecd0c45db7f6c0d7ff46c98b29be073d
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-24 23:21:58 +0100

    chore:fix colortheme

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,17 +1,52 @@
 return {
-  default_theme = {
-    highlights = function(hi)
-      local C = require "default_theme.colors"
-      hi.Normal = {bg = C.none, ctermbg = C.none}
-      hi.CursorColumn = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-      hi.CursorLine = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-      hi.CursorLineNr = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-      hi.LineNr = {}
-      hi.SignColumn = {}
-      hi.StatusLine = {}
-      hi.NeoTreeNormal = {bg = C.none, ctermbg = C.none}
-      hi.NeoTreeNormalNC = {bg = C.none, ctermbg = C.none}
-      return hi
+  plugins = {
+    heirline = function(config)
+      -- the first element of the default configuration table is the statusline
+      config[1] = {
+        -- set the fg/bg of the statusline
+        hl = { fg = "fg", bg = "bg" },
+        -- when adding the mode component, enable the mode text with padding to the left/right of it
+        astronvim.status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+        -- add all the other components for the statusline
+        astronvim.status.component.git_branch(),
+        astronvim.status.component.file_info(),
+        astronvim.status.component.git_diff(),
+        astronvim.status.component.diagnostics(),
+        astronvim.status.component.fill(),
+        astronvim.status.component.cmd_info(),
+        astronvim.status.component.fill(),
+        astronvim.status.component.lsp(),
+        astronvim.status.component.treesitter(),
+        astronvim.status.component.nav(),
+      }
+      -- return the final configuration table
+      return config
     end,
+  },
+  default_theme = {
+    colors = {
+      bg = "NONE"
+    }
+  },
+  highlights = {
+    -- apply highlight group to all colorschemes (include the default_theme)
+    init = {
+      -- set the transparency for all of these highlight groups
+      Normal = { bg = "NONE", ctermbg = "NONE" },
+      NormalNC = { bg = "NONE", ctermbg = "NONE" },
+      CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      LineNr = {},
+      SignColumn = {},
+      NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+      NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+      NotifyBackground = { bg = "#000000" }
+    },
+  },
+  options = {
+    opt = {
+      wrap = true
+    }
   },
 }
