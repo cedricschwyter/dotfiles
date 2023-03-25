d4ca929d287cfecb12dd28c8db67be9a25e45da0
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-03-25 13:47:37 +0100

    feat:add language servers

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -1,6 +1,10 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'jdtls' }
+    ensure_installed = { 'lua_ls', 'jdtls', 'rust_analyzer', 'clangd', 'cmake', 'ansiblels', 'asm_lsp', 'bashls',
+        'cssls', 'dockerls', 'docker_compose_language_service', 'gopls', 'gradle_ls', 'graphql', 'groovyls', 'html',
+        'hls', 'jsonls', 'kotlin_language_server', 'ltex', 'marksman', 'opencl_ls', 'spectral',
+        'perlnavigator', 'pylsp', 'sqlls', 'svelte', 'taplo', 'tailwindcss', 'tsserver', 'terraformls', 'vuels',
+        'lemminx', 'yamlls', 'zls' }
 })
 
 local lsp_defaults = require('lspconfig').util.default_config
@@ -37,3 +41,69 @@ require('lspconfig').jdtls.setup({
         jvm_args = '-javaagent:' .. install_path .. '/lombok.jar'
     }
 })
+
+require('lspconfig').rust_analyzer.setup({})
+
+require('lspconfig').clangd.setup({})
+
+require('lspconfig').cmake.setup({})
+
+require('lspconfig').ansiblels.setup({})
+
+require('lspconfig').asm_lsp.setup({})
+
+require('lspconfig').bashls.setup({})
+
+require('lspconfig').cssls.setup({})
+
+require('lspconfig').dockerls.setup({})
+
+require('lspconfig').docker_compose_language_service.setup({})
+
+require('lspconfig').gopls.setup({})
+
+require('lspconfig').gradle_ls.setup({})
+
+require('lspconfig').graphql.setup({})
+
+require('lspconfig').groovyls.setup({})
+
+require('lspconfig').html.setup({})
+
+require('lspconfig').hls.setup({})
+
+require('lspconfig').jsonls.setup({})
+
+require('lspconfig').kotlin_language_server.setup({})
+
+require('lspconfig').ltex.setup({})
+
+require('lspconfig').marksman.setup({})
+
+require('lspconfig').opencl_ls.setup({})
+
+require('lspconfig').spectral.setup({})
+
+require('lspconfig').perlnavigator.setup({})
+
+require('lspconfig').pylsp.setup({})
+
+require('lspconfig').sqlls.setup({})
+
+require('lspconfig').svelte.setup({})
+
+require('lspconfig').taplo.setup({})
+
+require('lspconfig').tailwindcss.setup({})
+
+require('lspconfig').tsserver.setup({})
+
+require('lspconfig').terraformls.setup({})
+
+require('lspconfig').vuels.setup({})
+
+require('lspconfig').lemminx.setup({})
+
+require('lspconfig').yamlls.setup({})
+
+require('lspconfig').zls.setup({})
