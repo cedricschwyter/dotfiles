5cb6098298dd76ece03a7b35828c9be27f8b893c
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-03-26 00:24:54 +0100

    feat:add line numbering

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -6,6 +6,8 @@ vim.opt.guicursor = 'n-v-c-sm:block'
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
