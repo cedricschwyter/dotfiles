be0a2425fbc5ad388e31eb32b88935527dced451
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-04-20 13:59:30 +0200

    feat:telesccope send references to quickfix

--- .config/nvim/lua/core/plugins/telescope.lua
+++ .config/nvim/lua/core/plugins/telescope.lua
@@ -1,6 +1,18 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            }
+        }
+    }
+})
