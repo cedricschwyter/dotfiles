e0f4a1d57933eefb1cc475e6443f6d297b2a32c9
Cedric Schwyter <cedric.schwyter@sitrox.com>
2023-09-06 09:23:03 +0200

    feat: integrate pass

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,8 @@
 config.load_autoconfig()
 config.source("qutebrowser-themes/themes/onedark.py")
 config.bind("p", "open -p {url}")
+
+config.bind('<z><l>', 'spawn --userscript qute-pass -U secret -u "login: (.+)" -d dmenu')
+config.bind('<z><u><l>', 'spawn --userscript qute-pass -U secret -u "login: (.+)" -e -d dmenu')
+config.bind('<z><p><l>', 'spawn --userscript qute-pass -U secret -u "login: (.+)" -w -d dmenu')
+config.bind('<z><o><l>', 'spawn --userscript qute-pass --otp-only -d dmenu')
