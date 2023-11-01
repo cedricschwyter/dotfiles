42b8152de6a00a0d2c83540df1bec4cef9af4e3b
Cedric Schwyter <cedric.schwyter@sitrox.com>
2023-11-01 15:43:55 +0100

    feat: quit nvim-tree on file open, telescope show hidden files

--- .config/nvim/lua/core/plugins/nvim-tree.lua
+++ .config/nvim/lua/core/plugins/nvim-tree.lua
@@ -2,10 +2,15 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require('nvim-tree').setup({
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        },
+    },
     git = {
         enable = true,
         ignore = false,
-    }
+    },
 })
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
--- .config/nvim/lua/core/plugins/telescope.lua
+++ .config/nvim/lua/core/plugins/telescope.lua
@@ -2,7 +2,8 @@ local telescope = require('telescope')
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<cr>",
+    { noremap = true })
 vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
@@ -14,6 +15,6 @@ telescope.setup({
             i = {
                 ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
             }
-        }
+        },
     }
 })
