dd3adacca70f162535547a2c38fef8bdbc2ef8d5
Cedric Schwyter <cedric@schwyter.io>
2023-11-17 16:15:12 +0100

    feat: add sqlfluff

--- .config/nvim/lua/core/mason.lua
+++ .config/nvim/lua/core/mason.lua
@@ -3,7 +3,7 @@ require('mason-lspconfig').setup({
     ensure_installed = { 'lua_ls', 'jdtls', 'rust_analyzer', 'clangd', 'cmake', 'ansiblels', 'asm_lsp', 'bashls',
         'cssls', 'dockerls', 'docker_compose_language_service', 'gopls', 'gradle_ls', 'graphql', 'groovyls', 'html',
         'hls', 'jsonls', 'kotlin_language_server', 'ltex', 'marksman', 'opencl_ls', 'spectral', 'perlnavigator', 'pylsp',
-        'sqlls', 'svelte', 'taplo', 'tailwindcss', 'tsserver', 'terraformls', 'vuels', 'lemminx', 'yamlls', 'zls' }
+        'svelte', 'taplo', 'tailwindcss', 'tsserver', 'terraformls', 'vuels', 'lemminx', 'yamlls', 'zls' }
 })
 
 local lsp_defaults = require('lspconfig').util.default_config
