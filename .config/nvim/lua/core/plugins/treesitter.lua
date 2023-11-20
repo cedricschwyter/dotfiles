4ed41360e2a412bdbcb687d30aa103c11f1d5694
Cedric Schwyter <cedric@schwyter.io>
2023-11-21 00:51:11 +0100

    feat: incremental treesitter selection

--- .config/nvim/lua/core/plugins/treesitter.lua
+++ .config/nvim/lua/core/plugins/treesitter.lua
@@ -1,9 +1,16 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'c', 'lua', 'rust', 'ruby', 'vim' },
+    ensure_installed = { 'c', 'lua', 'rust', 'ruby', 'vim' },
 
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<C-Space>",
+            node_incremental = "<C-Space>",
+        }
+    }
 })
