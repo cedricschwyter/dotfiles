a9b78a5663390b7a92cb19940c2f6b3f71010b42
cschwyter <cedric.schwyter@sitrox.com>
2025-11-18 15:08:05 +0100

    Fix breaking upgrade
    Signed-off-by: cschwyter <cedric.schwyter@sitrox.com>

--- .config/nvim/lua/core/mason.lua
+++ .config/nvim/lua/core/mason.lua
@@ -1,36 +1,60 @@
-require('mason').setup()
-require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'jdtls', 'rust_analyzer', 'clangd', 'cmake', 'ansiblels', 'asm_lsp', 'bashls',
-        'cssls', 'dockerls', 'docker_compose_language_service', 'eslint', 'gopls', 'gradle_ls', 'graphql', 'groovyls',
-        'html', 'hls', 'jsonls', 'kotlin_language_server', 'marksman', 'opencl_ls', 'spectral', 'perlnavigator', 'pylsp',
-        'svelte', 'taplo', 'tailwindcss', 'terraformls', 'vuels', 'lemminx', 'yamlls', 'zls', 'ruby_lsp' },
-    automatic_enable = true
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"jdtls",
+		"rust_analyzer",
+		"clangd",
+		"cmake",
+		"ansiblels",
+		"asm_lsp",
+		"bashls",
+		"cssls",
+		"dockerls",
+		"docker_compose_language_service",
+		"eslint",
+		"gopls",
+		"gradle_ls",
+		"graphql",
+		"groovyls",
+		"html",
+		"hls",
+		"jsonls",
+		"kotlin_language_server",
+		"marksman",
+		"opencl_ls",
+		"spectral",
+		"perlnavigator",
+		"pylsp",
+		"svelte",
+		"taplo",
+		"tailwindcss",
+		"terraformls",
+		"vuels",
+		"lemminx",
+		"yamlls",
+		"zls",
+		"ruby_lsp",
+	},
+	automatic_enable = true,
 })
-require('null-ls').setup()
-require('mason-null-ls').setup({
-    automatic_setup = true
+require("null-ls").setup()
+require("mason-null-ls").setup({
+	automatic_setup = true,
 })
 
-local lsp_defaults = require('lspconfig').util.default_config
-
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-    'force',
-    lsp_defaults.capabilities,
-    require('cmp_nvim_lsp').default_capabilities()
-)
-
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 
 local on_attach = function(_, _)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'LSP actions',
-    callback = on_attach
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = on_attach,
 })
--- .config/nvim/lua/core/packer.lua
+++ .config/nvim/lua/core/packer.lua
@@ -1,69 +1,71 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'ellisonleao/gruvbox.nvim'
-    use 'nvim-tree/nvim-tree.lua'
-    use 'nvim-tree/nvim-web-devicons'
-    use 'nvim-lualine/lualine.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
-            { 'nvim-lua/plenary.nvim' },
-            { "nvim-telescope/telescope-live-grep-args.nvim" },
-        },
-        config = function()
-            require("telescope").load_extension("live_grep_args")
-        end
-    }
-    use {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-        'williamboman/mason-null-ls.nvim',
-        'nvimtools/none-ls.nvim',
-        'neovim/nvim-lspconfig',
-    }
-    use 'lewis6991/gitsigns.nvim'
-    use 'xiyaowong/transparent.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'akinsho/toggleterm.nvim'
-    use {
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'L3MON4D3/LuaSnip',
-        'rafamadriz/friendly-snippets'
-    }
-    use 'sindrets/diffview.nvim'
-    use {
-        'akinsho/flutter-tools.nvim',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use 'simrat39/rust-tools.nvim'
-    use 'terrortylor/nvim-comment'
-    use 'APZelos/blamer.nvim'
-    use 'mhartington/formatter.nvim'
-    use { 'iamcco/markdown-preview.nvim',
-        run = function() vim.fn["mkdp#util#install"]() end,
-    }
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("ellisonleao/gruvbox.nvim")
+	use("nvim-tree/nvim-tree.lua")
+	use("nvim-tree/nvim-web-devicons")
+	use("nvim-lualine/lualine.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		},
+		config = function()
+			require("telescope").load_extension("live_grep_args")
+		end,
+	})
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"williamboman/mason-null-ls.nvim",
+		"nvimtools/none-ls.nvim",
+		"neovim/nvim-lspconfig",
+	})
+	use("lewis6991/gitsigns.nvim")
+	use("xiyaowong/transparent.nvim")
+	use("windwp/nvim-autopairs")
+	use("akinsho/toggleterm.nvim")
+	use({
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+	})
+	use("sindrets/diffview.nvim")
+	use({
+		"akinsho/flutter-tools.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("terrortylor/nvim-comment")
+	use("APZelos/blamer.nvim")
+	use("mhartington/formatter.nvim")
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
